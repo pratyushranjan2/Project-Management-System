62cf9af75186f17af45f9ccf5b5bd155cd905ed5
* <*pratyushranjan02@gmail.com*>
2023-04-06 12:55:27 +0530

    delete project

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -4,11 +4,13 @@ import auth from "../middleware/auth.js";
 import {
     getProjects,
     addProject,
+    deleteProject,
 } from '../controllers/projects.js';
 
 const router = express.Router();
 
 router.get('/', getProjects);
 router.post('/', auth, addProject);
+router.delete('/:id', auth, deleteProject)
 
 export default router;
