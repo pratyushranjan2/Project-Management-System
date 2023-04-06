41ec516a861bcf55423030e22172bdd4f3c7be43
* <*pratyushranjan02@gmail.com*>
2023-04-06 12:17:06 +0530

    addProject request with auth

--- server/controllers/projects.js
+++ server/controllers/projects.js
@@ -10,3 +10,15 @@ export const getProjects = async (req,res) => {
         res.status(404).json( { message: error } );
     }
 };
+
+export const addProject = async (req,res) => {
+    try {
+        const project = req.body;
+        const newProject = new Project({ ...project, creator: req.userId });
+        await newProject.save();
+        res.status(201).json(newProject);
+    }
+    catch (error) {
+        res.status(409).json({ message: error });
+    }
+};
