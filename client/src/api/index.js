b771845ef73aea8b7c4a083980d4cf09dfc1664e
Mohd. Rizwan Shaikh <mohdrizwan.shaikh2112@gmail.com>
2023-04-06 10:14:33 +0530

    Implemented routes in frontend

---
+++ client/src/api/index.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const API = axios.create({ baseURL: 'http://localhost:3001' });
+
+API.interceptors.request.use((req) => {
+    if (localStorage.getItem('profile')) {
+        req.headers.authorization = `Bearer ${
+            JSON.parse(localStorage.getItem('profile')).token
+        }`;
+    }
+    return req;
+});
+
+export const fetchProjects = () => API.get('/projects');
+export const fetchProjectsBySearch = (searchQuery) =>
+    API.get(`/projects/search?searchQuery=${searchQuery.search}`);
+export const createProject = (newProject) => API.post('/projects', newProject);
+export const updateProject = (id, updatedProject) =>
+    API.patch(`/projects/${id}`, updatedProject);
+export const deleteProject = (id) => API.delete(`/projects/${id}`);
+export const applyProject = (id) => API.patch(`/projects/${id}/applyProject`);
+
+export const signIn = (formData) => API.post('/users/signin', formData);
+export const signUp = (formData) => API.post('/users/signup', formData);
