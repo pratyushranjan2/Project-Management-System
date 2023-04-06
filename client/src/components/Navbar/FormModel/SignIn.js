d7f96659837818dd71da4d6fea547a934e33d2b5
Mohd. Rizwan Shaikh <mohdrizwan.shaikh2112@gmail.com>
2023-04-06 13:04:26 +0530

    Implemented sign in and sign up

---
+++ client/src/components/Navbar/FormModel/SignIn.js
@@ -0,0 +1,177 @@
+import { useState, useEffect } from 'react';
+// eslint-disable-next-line no-unused-vars
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { gapi } from 'gapi-script';
+import { useDispatch } from 'react-redux';
+import { AUTH } from '../../../constants/actionTypes';
+import { signin } from '../../../actions/auth';
+
+export default function SignIn({ setShowForm, setFormType }) {
+    const [formData, setFormData] = useState({ email: '', password: '' });
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // console.log(formData);
+        dispatch(signin(formData));
+    };
+
+    // const googleSuccess = async (res) => {
+    //     // console.log(res);
+    //     // const result = res.profileObj // if res is not undefined => cannot get property profileObj of undefined
+    //     const result = res?.profileObj; // OK
+    //     const token = res?.tokenId;
+
+    //     try {
+    //         dispatch({ type: AUTH, data: { result, token } });
+    //         window.location.reload();
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // };
+
+    // const googleFailure = () =>
+    //     console.log('Google Sign In was unsuccessful. Try Again Later');
+
+    useEffect(() => {
+        // function start() {
+        //     gapi.client.init({
+        //         clientId:
+        //             '252422663262-6k1s6rua8o3q3mhplasfvlc7ttsmf2qh.apps.googleusercontent.com',
+        //         scope: 'email',
+        //     });
+        // }
+        // gapi.load('client:auth2', start);
+    }, []);
+
+    return (
+        <>
+            <div
+                tabIndex="-1"
+                aria-hidden="true"
+                className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+            >
+                <div className="relative p-4 w-full max-w-md h-full md:h-auto mx-auto">
+                    <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                        <button
+                            type="button"
+                            className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                            onClick={() => setShowForm(false)}
+                        >
+                            <svg
+                                className="w-5 h-5"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                    clipRule="evenodd"
+                                ></path>
+                            </svg>
+                        </button>
+                        <div className="py-6 px-6 lg:px-8">
+                            <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+                                Sign in to our platform
+                            </h3>
+                            <form className="space-y-6" onSubmit={handleSubmit}>
+                                <div>
+                                    <label
+                                        htmlFor="email"
+                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                                    >
+                                        Your email
+                                    </label>
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        id="email"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                        placeholder="name@company.com"
+                                        onChange={(e) =>
+                                            setFormData({
+                                                ...formData,
+                                                email: e.target.value,
+                                            })
+                                        }
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <label
+                                        htmlFor="password"
+                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                                    >
+                                        Your password
+                                    </label>
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        id="password"
+                                        placeholder="••••••••"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                        onChange={(e) =>
+                                            setFormData({
+                                                ...formData,
+                                                password: e.target.value,
+                                            })
+                                        }
+                                        required
+                                    />
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                >
+                                    Login to your account
+                                </button>
+                                {/* <GoogleLogin
+                                    clientId="252422663262-6k1s6rua8o3q3mhplasfvlc7ttsmf2qh.apps.googleusercontent.com"
+                                    render={(renderProps) => (
+                                        <button
+                                            type="button"
+                                            onClick={renderProps.onClick}
+                                            disabled={renderProps.disabled}
+                                            className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 w-full justify-center"
+                                        >
+                                            <svg
+                                                className="w-4 h-4 mr-2 -ml-1"
+                                                aria-hidden="true"
+                                                focusable="false"
+                                                data-prefix="fab"
+                                                data-icon="google"
+                                                role="img"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 488 512"
+                                            >
+                                                <path
+                                                    fill="currentColor"
+                                                    d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+                                                ></path>
+                                            </svg>
+                                            Sign in with Google
+                                        </button>
+                                    )}
+                                    onSuccess={googleSuccess}
+                                    onFailure={googleFailure}
+                                    cookiePolicy="single_host_origin"
+                                /> */}
+                                <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+                                    Not registered?{' '}
+                                    <div
+                                        className="text-blue-700 hover:underline dark:text-blue-500 inline"
+                                        onClick={() => setFormType('SignUp')}
+                                    >
+                                        Create account
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="opacity-40 fixed inset-0 z-40 bg-black"></div>
+        </>
+    );
+}
