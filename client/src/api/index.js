c8d353ccba5c29f2e6a52efd9aa2aa0ce0d44124
* <*pratyushranjan02@gmail.com*>
2023-05-09 07:12:57 +0530

    changed server url for local testing

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: '' });
+const API = axios.create({ baseURL: 'http://localhost:3001' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
